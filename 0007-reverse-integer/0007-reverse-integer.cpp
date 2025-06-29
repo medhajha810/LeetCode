40d9fec4c352d0c443ccbec2a5be1690e6d8dcad
Medha jha <145251277+medhajha810@users.noreply.github.com>
2025-06-29 23:09:15 +0530

    Time: 0 ms (100%), Space: 8.3 MB (99.95%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -6,6 +6,9 @@ public:
         int rev=0;
         while(n!=0){
             rem=n%10;
+            if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && rem > 7)) return 0;
+            if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && rem < -8)) return 0;
+
             rev=(rev*10)+rem;
             n=n/10;
         }
