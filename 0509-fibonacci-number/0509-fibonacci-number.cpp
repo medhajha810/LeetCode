adf32a1a8a330d5925b9fd9e8872e64a3732d301
Medha jha <145251277+medhajha810@users.noreply.github.com>
2025-07-14 09:42:55 +0530

    Time: 3 ms (66.64%), Space: 7.9 MB (21.74%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -4,7 +4,7 @@ public:
         if(n==0){
             return 0;
         }
-        vector<int>arr(n+1);
+        int arr[n+1];
         arr[0]=0;
         arr[1]=1;
         for(int i=2;i<=n;i++){
