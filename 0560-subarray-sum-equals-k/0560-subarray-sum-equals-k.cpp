fbb875210c7da436808be3ae662010f3da99223f
Medha jha <145251277+medhajha810@users.noreply.github.com>
2026-01-24 10:55:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
-    int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int>SumCount;
-        SumCount[0]=1;
-        int sum=0, count=0;
+    int subarraySum(vector<int>& arr, int k) {
+        int n = arr.size();
+        int i = 0, j = 0;
+        long long sum = 0;
+        int ans = 0;
 
-        for (int num : nums) {
-            sum+=num;
-            if (SumCount.count(sum-k)) {
-                count+=SumCount[sum-k];
+        while (j < n) {
+            sum += arr[j];
+
+            while (i <= j && sum > k) {
+                sum -= arr[i];
+                i++;
             }
 
-            SumCount[sum]++;
+            if (sum == k) {
+                ans = max(ans, j - i + 1);
+            }
+            j++;
         }
-
-        return count;
+        return ans;
     }
 };
